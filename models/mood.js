17a06d4e644414f4ee3ef1b1f893054345be8573
Luis Rojas <lmrojasvilla@gmail.com>
2026-02-09 16:00:47 -0500

    changed rating back from 10 to 5

--- models/mood.js
+++ models/mood.js
@@ -17,7 +17,7 @@ const moodSchema = new mongoose.Schema({
     rating: {
         type: Number,
         min: 1,
-        max: 10,
+        max: 5,
         required: true,
     },
     moodLabel: {
