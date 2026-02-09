05068176dddef1dde26a288f57b96fd59d713386
TracywLan <tracylan12@gmail.com>
2026-02-09 12:55:01 -0500

    updated with new main

--- controllers/mood.js
+++ controllers/mood.js
@@ -3,6 +3,38 @@ const Mood = require('../models/mood');
 const router = express.Router();
 const verifyToken = require('../middleware/verify-token');
 
+// CREATE - POST /moods
+router.post('/', verifyToken, async (req,res) => {
+    try {
+        const moodData = {
+            ...req.body,
+            author:req.user._id,
+        }
+        const newMood = await Mood.create(moodData)
+
+        res.status(201).json(newMood);
+    } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: error.message }) 
+    }
+})
+
+//SHOW
+router.get("/:id", async (req,res) => {
+    try {
+        const mood = await Mood.findById(req.params.id)
+        .populate("author")
+        .populate("comments.author")
+        if(!mood){
+            return res.status(404).json({ error: "Mood not found "})
+        }
+        res.json(mood);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message })
+    }
+})
+
 // GET /moods/:moodId show
 router.get('/:moodId', async (req, res) => {
     try {
