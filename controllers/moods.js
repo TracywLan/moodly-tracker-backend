d4d0e053a509de13afb163e8aad13aa0b063b332
TracywLan <tracylan12@gmail.com>
2026-02-09 11:31:44 -0500

    completed the update route

--- controllers/moods.js
+++ controllers/moods.js
@@ -3,7 +3,7 @@ const Mood = require('../models/mood');
 const router = express.Router();
 const verifyToken = require('../middleware/verify-token');
 
-// GET /hoots/:hootId
+// GET /hoots/:hootId show
 router.get('/:moodId', async (req, res) => {
     try {
        const mood = await Mood.findById(req.params.moodId).populate('author');
@@ -13,13 +13,30 @@ router.get('/:moodId', async (req, res) => {
     }
 })
 
-// PUT /hoots/:hootId
-router.put('/:moodId', verifyToken, (req, res) => {
+// PUT /hoots/:hootId update
+router.put('/:moodId', verifyToken, async(req, res) => {
     try {
-        const mood = 
+        const mood = await Mood.findById(req.params.moodId);
+
+        if(!hoot.author.equals(req.user._id)) {
+            return res.status(403).send('You are not allowed to do that')
+        }
+
+        const updatedMood = await Mood.findByIdAndUpdate(
+            req.params.moodId,
+            req.body,
+            { new:true }
+        );
+        updatedMood._doc.author = req.author;
+
+        res.status(200).json(updatedMood)
+    } catch (err) {
+         res.status(500).json({ err: err.message });
     }
 })
 
+
+// DELETE /hoots/:hootId
 router.delete('/:moodId', async (req, res) => {
     try {
     await Mood.findByIdAndDelete(req, params.moodId);
